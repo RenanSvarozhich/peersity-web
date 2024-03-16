594b30790d1a178f1e57b23d18196b3f29ed0991
RenanSvarozhich <renan.svarozhich@starsaligned.tech>
2024-03-15 21:50:32 -0300

    Create Skills page

--- app/shared/navbar.tsx
+++ app/shared/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import NavbarLinks from "./navbar-links";
 
 export default function Navbar() {
   return (
@@ -22,7 +23,7 @@ export default function Navbar() {
         </div>
         <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
-            <Link className="font-medium text-orange-500 sm:py-6 dark:text-orange-500" href="/" aria-current="page">Home</Link>
+            <NavbarLinks />
           </div>
         </div>
       </nav>
---
+++ app/skills/page.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+
+export default function Skills() {
+  return (
+    <main id="content" role="main" className="max-w-7x1 shrink-0">
+      <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')]">
+        <div className="max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+          <div className="mt-5 max-w-2xl text-center mx-auto">
+            <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+              <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">Skills</span>              
+            </h1>
+          </div>
+
+          <div className="mt-5 max-w-3xl text-center mx-auto">
+            <p className="text-lg text-gray-600 dark:text-gray-400">Skills are modules that group learning resources by theme. </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-7x1 px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="grid sm:grid-cols-3 lg:grid-cols-6 gap-6">
+
+          <div className="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] transform transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-lg">
+            <div className="h-60 rounded-t-xl bg-[url('/skills/c.webp')] bg-cover bg-no-repeat bg-center">
+            </div>
+            <div className="p-4 md:p-6">
+              <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                C language
+              </h3>
+              <p className="mt-3 text-gray-500">
+                A software that develops products for software developers and developments.
+              </p>
+            </div>
+          </div>
+
+          <div className="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] transform transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-lg">
+            <div className="h-60 rounded-t-xl bg-[url('/skills/java.webp')] bg-cover bg-no-repeat bg-center">
+            </div>
+            <div className="p-4 md:p-6">
+              <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                Java language
+              </h3>
+              <p className="mt-3 text-gray-500">
+                A software that develops products for software developers and developments.
+              </p>
+            </div>
+          </div>
+
+          <div className="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] transform transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-lg">
+            <div className="h-60 rounded-t-xl bg-[url('/skills/rust.webp')] bg-cover bg-no-repeat bg-center">
+            </div>
+            <div className="p-4 md:p-6">
+              <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                Rust language
+              </h3>
+              <p className="mt-3 text-gray-500">
+                A software that develops products for software developers and developments.
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  );
+}
